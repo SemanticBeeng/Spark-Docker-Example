a9b9c67214f5a0e4e46928445740a6de42cbe918
SemanticBeeng <nick@semanticbeeng.com>
2017-05-29 14:53:46 -0700

    adding mysql jar to Spark query core-db
    works if using this from host
    ```
    sshuttle -l 0.0.0.0 --dns -r nick@o.scivulcan.com:5500 192.81.130.200/32
    ```
    "java.lang.ClassNotFoundException: com.mysql.jdbc.Driver"
    
    add mysql dependency to original semantive images
    
    adding mysql jdbc connector to application itself (instead)
    
    runs !
    
    run on `http://ds-db3.scivulcan.com`
     Job 0 finished: foreach at SparkApplication.scala:69, took 275.692546 s
    
    another run for the `#table_citation` : fails at the end
    "Lost task 999.0 in stage 0.0 (TID 999, 172.17.0.4, executor 0): java.lang.OutOfMemoryError: GC overhead limit exceeded"
    
    this version results in this error quite quickly: likely tried to bring large results in RAM
    " Lost task 66.0 in stage 0.0 (TID 66, 172.17.0.4, executor 0): java.lang.OutOfMemoryError: GC overhead limit exceeded"
    
    previous version ran successfully on `#table_citation`
    
    attempts to fix the memory related errors at the end
    https://www.slideshare.net/jcmia1/a-beginners-guide-on-troubleshooting-spark-applications
    
    trying GC1

--- sbt-application/src/main/scala-2.11/SparkApplication.scala
+++ sbt-application/src/main/scala-2.11/SparkApplication.scala
@@ -22,10 +22,11 @@ object SparkApplication {
       .set("spark.driver.memory", "3g")
       // https://stackoverflow.com/questions/37260230/spark-cluster-full-of-heartbeat-timeouts-executors-exiting-on-their-own
       .set("spark.network.timeout", 300.seconds.toMillis.toString)
-      .set("spark.executor.extraJavaOptions", "-XX:+UseG1GC -XX:+PrintFlagsFinal -XX:+PrintReferenceGC -verbose:gc " +
+      // https://databricks.com/blog/2015/05/28/tuning-java-garbage-collection-for-spark-applications.html
+      .set("spark.executor.extraJavaOptions", "-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35 -XX:ConcGCThread=20 " +
+                                              "-XX:+PrintFlagsFinal -XX:+PrintReferenceGC -verbose:gc " +
                                               "-XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintAdaptiveSizePolicy " +
-                                              "-XX:+UnlockDiagnosticVMOptions -XX:+G1SummarizeConcMark " +
-                                              "XX:InitiatingHeapOccupancyPercent=35 -XX:ConcGCThread=20")
+                                              "-XX:+UnlockDiagnosticVMOptions -XX:+G1SummarizeConcMark ")
 
     val session = SparkSession
       .builder
